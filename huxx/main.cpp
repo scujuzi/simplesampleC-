7e50339bc51b2ccdae4e962ebf1335c5ad307f9d
scujuzi <736401010@qq.com>
2018-11-08 14:05:36 +0800

    简单C++

---
+++ huxx/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stdio.h>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+int main()
+{
+	int i=1;
+	printf("%d",(++i)+(++i));
+
+////const int a=1;
+////int *b=(int *)&a;
+////int c=3;
+////const int *d=&c;
+////*b=2;
+////c=4;
+////printf("a=%d,b=%d,c=%d,d=%d",a,*b,c,*d);
+////return 0;
+//
+//	/*int i=1;
+//	int j=i++;
+//	if((i>j++)&&(i++==j))
+//		i+=j;
+//printf("i=%d",i);*/
+//	int x=0;
+//	int y=0;
+//	while(x<15)
+//		y++,x+=++y;
+//	printf("%d,%d",y,x);
+	system("pause");
+//
+}
